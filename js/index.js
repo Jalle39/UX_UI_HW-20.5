61b8b6ea93287ca8ad1f1b88e25bb47d5b5acbb6
Jalle39 <107419181+Jalle39@users.noreply.github.com>
2022-11-17 17:18:33 -0700

    prime

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(funtion(){
+  $("a").on('click', funtion(click))
+    if (this. hash !=="pageTop"){
+      event.preventDefault();
+      var hash= this.hash
+    });
